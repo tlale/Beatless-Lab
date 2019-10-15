ba4e223a098d11277eba4985430b4641b40bf383
tlale <54483909+tlale@users.noreply.github.com>
2019-10-14 23:55:00 -0500

    beatlesindex.js
    using loops

---
+++ index.js
@@ -0,0 +1,48 @@
+//flatiron beatles lab by Tlale
+
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+// for loop
+
+function theBeatlesPlay( musician, instruments){
+    var allPlayers = [];
+    for (var i = 0; i < players.length; i++)
+    allPlayers.push(players[i] + " plays " + instruments[i]);
+    }
+    return allPlayers;
+}
+    
+    const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+// while loop
+
+function johnLennonFacts(facts) {
+   var allPlayers=[];
+   let i = 0;
+   while(facts.length > i){
+    arr.push(`${facts[i]}!!!`)
+    i++
+  }
+   return allPlayers();
+   }    
+   
+   function iloveTheBeats(number){
+    const array=[];
+    do{
+        array.push("I love the Beatles")
+        number++;
+    } while (number <15)
+    
+        return array
+}
+           
+   
+   
+   
